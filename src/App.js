9ca0d46af2125012d564a76372824fc55ad56fe6
leejunghyeok <wndgur76@naver.com>
2021-11-19 16:56:50 +0900

    c

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import logo from './logo.svg';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My first React app!</h1>
+      <p>Welcome :)</p>
     </div>
   );
 }
