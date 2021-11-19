8aa396a118e6908f0e5f1641484086b35c4515f5
leejunghyeok <wndgur76@naver.com>
2021-11-19 20:26:39 +0900

    Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React app!</h1>
-      <p>Welcome :)</p>
-    </div>
-  );
+import React, {Component} from 'react';
+import Ninjas from "./Ninjas";
+import AddNinja from './addNinja';
+
+class App extends Component {
+  state = {
+    ninjas : [
+      {name: 'Lee', age: 30, belt: 'black', id:1 },
+      {name: 'Park', age: 16, belt: 'yellow', id:2 },
+      {name: 'Lim', age: 12, belt: 'green', id:3 }
+    ]
+  }
+  addNinja = (ninja)=>{
+    ninja.id=Math.floor(Math.random()*10000);
+    console.log(ninja);
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+        <AddNinja addNinja={this.addNinja}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Ninjas.js
@@ -0,0 +1,46 @@
+const Ninjas = ({ninjas}) => {
+  const ninjaList = ninjas.map(ninja => {
+    if(ninja.age>20){
+      return (
+        <div className="ninja" key={ninja.id}>
+          <div>Name: { ninja.name }</div>
+          <div>Age: { ninja.age }</div>
+          <div>Belt: { ninja.belt }</div>
+          <br/>
+        </div>
+      )
+    }else
+      return null;
+  });
+  // const ninjaList = ninjas.map(ninja => {
+  //   return ninja.age>20 ? (
+  //           <div className="ninja" key={ninja.id}>
+  //             <div>Name: { ninja.name }</div>
+  //             <div>Age: { ninja.age }</div>
+  //             <div>Belt: { ninja.belt }</div>
+  //             <br/>
+  //           </div>
+  //         ) : null;
+  // });
+  return(
+    <div className="ninja-list">
+      { ninjaList
+        /* {ninjas.map(ninja => {
+        if(ninja.age>20){
+          return (
+            <div className="ninja" key={ninja.id}>
+              <div>Name: { ninja.name }</div>
+              <div>Age: { ninja.age }</div>
+              <div>Belt: { ninja.belt }</div>
+              <br/>
+            </div>
+          )
+        }else
+          return null;
+      })
+    } */}
+    </div>
+  );
+}
+
+export default Ninjas;
